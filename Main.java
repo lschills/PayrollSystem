24cc7cd97512242961c0debfbd1ae1a78a3f43f6
Lauryn Schiller <98788167+lschills@users.noreply.github.com>
2022-03-28 13:37:00 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,41 +1,14 @@
 //Lauryn Schiller lauryns1@umbc.edu
-import java.util.Scanner;
-
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        int calculation;
-        do {
-            System.out.println("Input two numbers");
-            int num1 = myObj.nextInt();
-            int num2 = myObj.nextInt();
-            System.out.println("Press 0 to quit");
-            System.out.println("Press 1 to add");
-            System.out.println("Press 2 to subtract");
-            System.out.println("Press 3 to multiply");
-            System.out.println("Press 4 to divide");
-            calculation = myObj.nextInt();
-            switch (calculation) {
-                case 0:
-                    System.out.println("Calculation quit");
-                    break;
-                case 1:
-                    System.out.println(num1 + num2);
-                    break;
-                case 2:
-                    System.out.println(num1 - num2);
-                    break;
-                case 3:
-                    System.out.println(num1 * num2);
-                    break;
-                case 4:
-                    System.out.println((float) num1 / num2);
-                    break;
-                default:
-                    System.out.println("Error please enter a number 0-4");
+    public static void triangle(int height) {
+        for (int i = height; i >= 1; i = i - 1) {
+            System.out.println();
+            for (int j = i; j >= 1; j = j - 1) {
+                System.out.print("*");
             }
         }
-        while (calculation > 0);
+    }
+    public static void main(String[] args) {
+    triangle(10);
     }
 }
